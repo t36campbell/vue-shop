65eb1dc730b7b30b9e0d1832c8c4d488d99fb863
t36campbell <campbell3636@yahoo.com>
2020-06-21 13:39:42 -0400

    updated style of products section

--- src/components/products.vue
+++ src/components/products.vue
@@ -12,92 +12,134 @@
           </p>
         </div>
       </div>
-      <div class="row">
-        <div class="col-lg-4 col-md-6 mb-5">
-          <div class="product-item">
-            <figure>
-              <img src="../assets/images/imac.jpg" alt="Image" class="img-fluid" />
-            </figure>
-            <div class="px-4">
-              <h3>Apple iMac</h3>
-              <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-              <div>
-                <a href="#" class="btn btn-black btn-outline-black ml-1 rounded-0">View Item</a>
+      <b-card-group deck>
+        <div class="card bg-white border-dark rounded-0">
+          <img src="../assets/images/imac.jpg" alt="Image" class="card-img rounded-0" />
+          <div class="card-body">
+            <h3 class="card-title text-black mb-4 font-weight-bold text-center">Apple iMac</h3>
+            <p class="card-text mx-4 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+              nostrum natus excepturi fuga.
+            </p>
+            <div class="col-12 text-center">
+              <div class="mx-auto row form-group justify-content-center">
+                <br />
+                <a
+                  class="col-12 btn btn-black ml-1 rounded-0"
+                  href="#schedule"
+                  v-smooth-scroll
+                >View Item</a>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-6 mb-5">
-          <div class="product-item">
-            <figure>
-              <img src="../assets/images/ipads.jpg" alt="Image" class="img-fluid" />
-            </figure>
-            <div class="px-4">
-              <h3>Apple iPads</h3>
-              <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-              <div>
-                <a href="#" class="btn btn-black btn-outline-black ml-1 rounded-0">View Item</a>
+        <div class="card bg-white border-dark rounded-0">
+          <img src="../assets/images/ipads.jpg" alt="Image" class="card-img rounded-0" />
+          <div class="card-body">
+            <h3 class="card-title text-black mb-4 font-weight-bold text-center">Apple iPads</h3>
+            <p class="card-text mx-4 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+              nostrum natus excepturi fuga.
+            </p>
+            <div class="col-12 text-center">
+              <div class="mx-auto row form-group justify-content-center">
+                <br />
+                <a
+                  class="col-12 btn btn-black ml-1 rounded-0"
+                  href="#schedule"
+                  v-smooth-scroll
+                >View Item</a>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-6 mb-5">
-          <div class="product-item">
-            <figure>
-              <img src="../assets/images/iphone.jpg" alt="Image" class="img-fluid" />
-            </figure>
-            <div class="px-4">
-              <h3>Apple iPhone 6</h3>
-              <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-              <div>
-                <a href="#" class="btn btn-black btn-outline-black ml-1 rounded-0">View Item</a>
+      </b-card-group>
+      <br />
+      <b-card-group deck>
+        <div class="card bg-white border-dark rounded-0">
+          <img src="../assets/images/iphone.jpg" alt="Image" class="card-img rounded-0" />
+          <div class="card-body">
+            <h3 class="card-title text-black mb-4 font-weight-bold text-center">Apple iPhone 6</h3>
+            <p class="card-text mx-4 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+              nostrum natus excepturi fuga.
+            </p>
+            <div class="col-12 text-center">
+              <div class="mx-auto row form-group justify-content-center">
+                <br />
+                <a
+                  class="col-12 btn btn-black ml-1 rounded-0"
+                  href="#schedule"
+                  v-smooth-scroll
+                >View Item</a>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-6 mb-5">
-          <div class="product-item">
-            <figure>
-              <img src="../assets/images/ipads-2.jpg" alt="Image" class="img-fluid" />
-            </figure>
-            <div class="px-4">
-              <h3>Apple iPads</h3>
-              <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-              <div>
-                <a href="#" class="btn btn-black btn-outline-black ml-1 rounded-0">View Item</a>
+        <div class="card bg-white border-dark rounded-0">
+          <img src="../assets/images/ipads-2.jpg" alt="Image" class="card-img rounded-0" />
+          <div class="card-body">
+            <h3 class="card-title text-black mb-4 font-weight-bold text-center">Apple iPads</h3>
+            <p class="card-text mx-4 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+              nostrum natus excepturi fuga.
+            </p>
+            <div class="col-12 text-center">
+              <div class="mx-auto row form-group justify-content-center">
+                <br />
+                <a
+                  class="col-12 btn btn-black ml-1 rounded-0"
+                  href="#schedule"
+                  v-smooth-scroll
+                >View Item</a>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-6 mb-5">
-          <div class="product-item">
-            <figure>
-              <img src="../assets/images/macbook.jpg" alt="Image" class="img-fluid" />
-            </figure>
-            <div class="px-4">
-              <h3>Apple MacBook Pro</h3>
-              <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-              <div>
-                <a href="#" class="btn btn-black btn-outline-black ml-1 rounded-0">View Item</a>
+      </b-card-group>
+      <br />
+      <b-card-group deck>
+        <div class="card bg-white border-dark rounded-0">
+          <img src="../assets/images/macbook.jpg" alt="Image" class="card-img rounded-0" />
+          <div class="card-body">
+            <h3 class="card-title text-black mb-4 font-weight-bold text-center">Apple MacBook Pro</h3>
+            <p class="card-text mx-4 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+              nostrum natus excepturi fuga.
+            </p>
+            <div class="col-12 text-center">
+              <div class="mx-auto row form-group justify-content-center">
+                <br />
+                <a
+                  class="col-12 btn btn-black ml-1 rounded-0"
+                  href="#schedule"
+                  v-smooth-scroll
+                >View Item</a>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-6 mb-5">
-          <div class="product-item">
-            <figure>
-              <img src="../assets/images/watch.jpg" alt="Image" class="img-fluid" />
-            </figure>
-            <div class="px-6">
-              <h3>Apple Watch Series 3</h3>
-              <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-              <div>
-                <a href="#" class="btn btn-black btn-outline-black ml-1 rounded-0">View Item</a>
+        <div class="card bg-white border-dark rounded-0">
+          <img src="../assets/images/watch.jpg" alt="Image" class="card-img rounded-0" />
+          <div class="card-body">
+            <h3 class="card-title text-black mb-4 font-weight-bold text-center">Apple Watch Series 3</h3>
+            <p class="card-text mx-4 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+              nostrum natus excepturi fuga.
+            </p>
+            <div class="col-12 text-center">
+              <div class="mx-auto row form-group justify-content-center">
+                <br />
+                <a
+                  class="col-12 btn btn-black ml-1 rounded-0"
+                  href="#schedule"
+                  v-smooth-scroll
+                >View Item</a>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </b-card-group>
     </div>
   </div>
 </template>
